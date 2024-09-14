d5662b4f4ba624ea8092eadf148c75c7bd11216a
burn_in_soul <burn-in-soul@uandex.ru>
2024-09-14 17:10:47 +0300

    rm containerts

--- smm_vk/containers/vk_client.py
+++
@@ -1,8 +0,0 @@
-from smm_vk.clients.vk_client.vk_client import VkClientV1
-from aiovk import TokenSession
-
-
-class VkClientContainer:
-
-    def __init__(self, session: TokenSession):
-        self.client = VkClientV1(session)
